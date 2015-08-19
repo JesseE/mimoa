cc2150ba736a59fd10dcc6bbe872641810f057e1
Jesse Eikema <jesseeikema@Jesses-MacBook-Pro.local>
2015-08-19 10:02:53 +0200

    commited all files

---
+++ client/view/application/profile/my-profile-list-creation.js
@@ -0,0 +1,23 @@
+/**
+ * Created by jesseeikema on 8/18/15.
+ */
+Template.profileListCreation.helpers({
+
+});
+Template.profileListCreation.events({
+    'submit form':function(event){
+        event.preventDefault();
+        var currentUserId = Meteor.userId();
+        var listName = event.target.listNameInput.value;
+        Meteor.call('createFavList',listName,currentUserId,function(err,res){
+            if(err){throw err;} else{
+                return Router.go('/profile/'+currentUserId);
+            }
+        });
+
+        //$('.new-list').removeClass('fadeInRight');
+        //$('.new-list').addClass('fadeOutLeft');
+        //$('.post-container').removeClass('fadeOutLeft');
+        //$('.post-container').addClass('fadeInRight');
+    }
+});
